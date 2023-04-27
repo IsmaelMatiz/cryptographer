cdc32f77102e148bb92189676eab355d70a47151
IsmaelMatiz <ismaelmatiz@gmail.com>
2023-04-27 11:26:21 -0500

    function decrypt almost finished (some issues to decrypt the o)

--- index.js
+++ index.js
@@ -22,45 +22,99 @@ window.onload = () =>{mainInput.focus()}
 
 const isUpperCaseOrSpecialCharacter = (string) => /([A-Z])|([\\\/\(\)\{\}\[\]\!\"\#\$\%\&\=\?\¡\ñ\Ñ])/.test(string)
 
+mainInput.oninput = transcrypt
 
 
-mainInput.onkeyup = encrypter
-mainInput.onpaste = addEventListener('paste',(event)=>{
-    console.log(event.clipboardData.getData('text'))
-})
-//TODO: usar el texto pegado en el clipboard para desencryptarlo
+mainInput.onpaste = (event)=>{
+    const inputText = event.clipboardData.getData('text');
+    
+    setTimeout(() => {
+        deleteResult()
+        pastEncrypter(inputText);
+}, 10)}
 
-//TODO: probablemente reestructurar la funcion para pasar el texto como parametro
-function encrypter(){
-    if(isUpperCaseOrSpecialCharacter(mainInput.value)){
+
+function transcrypt(){
+    
+    inEncrypter(mainInput.value)
+}
+
+function inEncrypter(inputText){
+    if(isUpperCaseOrSpecialCharacter( inputText)){//Verificar mayusculas o caracteres especiales
         alert("no se permiten mayusculas o caracteres especiales")
     } else{
-        if(encryptOrDecrypt.value === "Encrypt"){
-            lastCharacter = mainInput.value.split('')[mainInput.value.length -1]
-            for(let i = 0;i < Object.keys(encryptKey).length;i++){
-                if(lastCharacter === Object.keys(encryptKey)[i]){
-                    result.innerText += encryptKey[Object.keys(encryptKey)[i]]
+        if(encryptOrDecrypt.value === "Encrypt"){//hacer switch entre encriptar y desencriptar, lo que elija el usuario
+            lastCharacter = inputText.split('')[inputText.length -1]
+            for(let vocal in encryptKey){
+                if(lastCharacter === vocal){//encriptar las vocales
+                    result.innerText += encryptKey[vocal]
                     vocalYes = true
                     break
                 }
             }
-            if(!vocalYes) {
-                result.innerText += lastCharacter
-            }
+            if(!vocalYes)result.innerText += lastCharacter//agregar palabras normales
             vocalYes = false
-            if(lastCharacter === " "){
-                console.log("Espaciooo")
-                result.innerText += '‍‍‍‍‍ㅤ'
-            }
+            if(lastCharacter === " ")result.innerText += '‍‍‍‍‍ㅤ'//agregar espacios
 
         }else{
+            lastCharacter = inputText.split('')[inputText.length -1]
+            result.innerText += lastCharacter
+            if(lastCharacter === " ")result.innerText += '‍‍‍‍‍ㅤ'//agregar espacios
+            for(let vocal in encryptKey){
+                result.innerText = result.innerText.replace(encryptKey[vocal],vocal)
+            }
         }
         console.log()
     }
 }
 
+
+function pastEncrypter(inputText){
+    if(isUpperCaseOrSpecialCharacter( inputText)){//Verificar mayusculas o caracteres especiales
+        alert("no se permiten mayusculas o caracteres especiales")
+    } else{
+        if(encryptOrDecrypt.value === "Encrypt"){//hacer switch entre encriptar y desencriptar, lo que elija el usuario
+            for (let letter in inputText) {
+                let eachLetter = inputText[letter]
+                for(let vocal in encryptKey){
+                    if(eachLetter === vocal){//encriptar las vocales
+                        result.innerText += encryptKey[vocal]
+                        vocalYes = true
+                        break
+                    }
+                }
+                if(!vocalYes)result.innerText += eachLetter//agregar palabras normales
+                vocalYes = false
+                if(eachLetter === " ")result.innerText += '‍‍‍‍‍ㅤ'//agregar espacios
+                
+            }
+        }else{
+            for (let letter in inputText) {
+                let eachLetter = inputText[letter]
+                result.innerText += eachLetter//agregar palabras normales
+                vocalYes = false
+                if(eachLetter === " ")result.innerText += '‍‍‍‍‍ㅤ'//agregar espacios             
+            }
+
+            //TODO: arreglar problema con la o
+            let replaceResult = result.innerText + ""
+            console.log("antes del for: "+replaceResult)
+            for (const vocal in encryptKey) {
+                console.log(encryptKey[vocal]+" vs "+vocal)
+                replaceResult = replaceResult.replace(encryptKey[vocal],vocal)    
+            }
+            
+            result.innerText = replaceResult
+            console.log("pero q ha pasao: "+replaceResult)
+        }
+    }
+
+}
+
+
+
 function deleteResult(){
-    console.log("BORRRAAAAARRRR")
     result.innerText = ""
 }
 
+//TODO: hacer funcion para copiar el texto
